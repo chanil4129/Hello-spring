ccdaaca115e404c4f415e0cf3a87737d01b83111
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-08-06 01:30:21 +0900

    6. 스프링 데이터 JPA

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -24,7 +24,7 @@ class MemberServiceIntegrationTest {
     void join() {
         //given
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring3");
 
         //when
         Long saveId = memberService.join(member);
@@ -38,10 +38,10 @@ class MemberServiceIntegrationTest {
     public void 중복_회원_예외() {
         //given
         Member member1 = new Member();
-        member1.setName("spring");
+        member1.setName("spring3");
 
         Member member2 = new Member();
-        member2.setName("spring");
+        member2.setName("spring3");
 
         //when
         memberService.join(member1);
